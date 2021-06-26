9cdc352244853755e180e9b33a188979d19dcd86
Ernesto Ianuario <ernestoianuario@Ernestos-iMac.lan>
2021-06-26 10:48:00 +0100

    Remove console warnings

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// remove console warnings
+console.warn = () => {}
 
 ReactDOM.render(
   <React.StrictMode>
